98f407e8302b2f77581e2c2dffb29918be51a0ab
Andrew Becker <57923812+arbecker620@users.noreply.github.com>
2024-08-14 10:46:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 
 from Directory import Directory
 from Summary import Summary
+from Package import Package,Package_Exectution, Package_MetaData
 
 #Output Results directory
 #assign Direcotry to Output Results
@@ -34,24 +35,22 @@ def main(file_dir: str):
 
 
     for ssis_package in file_directory.ssis_files:
+        #package = Package(ssis_package, file_dir)
 
-        xmlTree = ET.parse(ssis_package)
-        elemList = []
     # now I remove duplicities - by convertion to set and back to list
-
+        
         tree = etree.parse(ssis_package)
         root = tree.getroot()
  
         total_bytes = 0
 
         list_properties = []
-        list_path_info = []
-        list_connection_strings = []
-        list_sql_statements = []
+        package_meta = Package_MetaData(ssis_package, file_dir)
+
+        package_exec = Package_Exectution(ssis_package,file_dir)
 
         for cnt, ele in tqdm(enumerate(root.xpath(".//*"))):
 
-            ###Get SQL Code
             if ele.tag == "{www.microsoft.com/SqlServer/Dts}Executable": 
                 for child0 in ele:
                     if child0.tag == "{www.microsoft.com/SqlServer/Dts}ObjectData":
@@ -60,80 +59,95 @@ def main(file_dir: str):
                             if child1.tag == "{www.microsoft.com/sqlserver/dts/tasks/sqltask}SqlTaskData":
                                 dtsx_sql = child1.attrib["{www.microsoft.com/sqlserver/dts/tasks/sqltask}SqlStatementSource"]
                                 dtsx_sql = "-- " + sql_comment + "\n" + dtsx_sql
-                                #mdFile.new_paragraph(dtsx_sql)
-                                list_sql_statements.append(dtsx_sql)
+
+                                package_exec.list_sql_statements.append(dtsx_sql)
+                                
             elif ele.tag== 'path':
-                #print(ele.tag)
                 dict_path = dict(ele.attrib)
                 path_summary = []
                 for k,v in dict_path.items():
                     line = str(k)+" : "+str(v)
                     path_summary.append(line)
-                #print(list_path_info)
-                #take key and value, combin into a string, then add a new line to string then
-                
-                #print(ele.attrib[0][0])
-                list_path_info.append(path_summary)
-                #list_path_info= "\n".join(path_summary)
+
+                package_exec.list_path_info.append(path_summary)
+
 
             elif ele.tag == 'output' or ele.tag == 'outputColumn':  #finish getting data from here
-                print("")
-
-                #print(ele.attrib)
-            elif ele.tag == 'inputColumn' or ele.tag == 'input': #finish data from here
-                print("")
-                #print(ele.text)
-                #print(ele.attrib)
-            elif ele.tag == 'property':
-                print("")
-                #print(ele.attrib)
-                #print(ele.text)
-            elif ele.tag == 'connection' or ele.tag == 'ConnectionManager':
-                print("Connection1")
-
-                print(ele.attrib)
-                #print(ele.text)
-            elif ele.tag == "{www.microsoft.com/SqlServer/Dts}Property":
-                #print("property")
-                #print(ele.attrib)
-
                 dict_path = dict(ele.attrib)
-                #dict_text = dict(ele.text)
+                output_summary = []
+                for k,v in dict_path.items():
+                    line = str(k)+" : "+str(v)
+                    output_summary.append(line)
+
+                package_exec.list_output_columns.append(output_summary)
+
+            elif ele.tag == 'inputColumn' or ele.tag == 'input': #finish data from here
+                dict_path = dict(ele.attrib)
+                input_summary = []
+                for k,v in dict_path.items():
+                    line = str(k)+" : "+str(v)
+                    input_summary.append(line)
+
+                package_exec.list_input_columns.append(input_summary)
+
+            elif ele.tag == 'property':
+                print(ele.attrib)
+            elif ele.tag == 'connection' or ele.tag == 'ConnectionManager':
+                dict_path = dict(ele.attrib)
+                connection_summary = []
+                for k,v in dict_path.items():
+                    line = str(k)+" : "+str(v)
+                    connection_summary.append(line)
+
+                package_exec.list_connection_info.append(connection_summary)
+
+            elif ele.tag == "{www.microsoft.com/SqlServer/Dts}Property":
+                
+                dict_path = dict(ele.attrib)
                 for i, v  in dict_path.items():
                     if i == '{www.microsoft.com/SqlServer/Dts}Name' and v == 'ConnectionString':
-                        list_connection_strings.append(ele.text)
+                        package_exec.list_connection_strings.append(ele.text)
                         
-
-
-                    #print(dict_path,ele.text)
-                #print(dict_text)
-
-                #list_properties.append(ele.attrib)
-                #list_properties.append(ele.text)
-
-                #print(ele.attrib)
-
-                    #for child1 in child0:
-                    #    print(child1)
-                    #for child1 in child0:
-                    #    print(child1.tag)
         
+        mdFile.new_header(level=1, title=ssis_package)
+
         
         mdFile.new_header(level=1, title="Paths")
         mdFile.new_paragraph("The information below depicts the data flows or paths which is being used inside of the SSIS Package.")
-        for path in list_path_info:
+        for path in package_exec.list_path_info:
             summary="\n".join(path)
             mdFile.new_paragraph(summary)
 
+        mdFile.new_header(level=1, title="Connection Information")
+        mdFile.new_paragraph("This is the identified Connection Types in inside of this SSIS Package. \nRefer to the Connection String section to see where the SSIS package is connected to. ")
+        for connection in  package_exec.list_connection_info:
+            summary="\n".join(connection)
+            mdFile.new_paragraph(summary)
+
+
         mdFile.new_header(level=1, title="Connection Strings")
         mdFile.new_paragraph("This is the identified Connection Strings inside of this SSIS Package")
-        for connection in list_connection_strings:
+        for connection in  package_exec.list_connection_strings:
             summary="".join(connection)
+            mdFile.new_paragraph(summary)
+
+
+        mdFile.new_header(level=1, title="Input Columns")
+        mdFile.new_paragraph("This is the identified Input Columns inside of this SSIS Package")
+        for input_cols in  package_exec.list_input_columns:
+            summary="\n".join(input_cols)
+            mdFile.new_paragraph(summary)
+
+
+        mdFile.new_header(level=1, title="Output Columns")
+        mdFile.new_paragraph("This is the identified Output Columns inside of this SSIS Package")
+        for connection in  package_exec.list_output_columns:
+            summary="\n".join(connection)
             mdFile.new_paragraph(summary)
 
         mdFile.new_header(level=1, title="SQL Statements inside ExecutableFile")
         mdFile.new_paragraph("This is the SQL Statement inside of the Executable")
-        for Statements in list_sql_statements:
+        for Statements in package_exec.list_sql_statements:
             summary="".join(Statements)
             mdFile.new_paragraph(summary)
 
